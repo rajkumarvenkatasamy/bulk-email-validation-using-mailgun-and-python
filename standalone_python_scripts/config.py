d7870eb7e938a01d560e7478f5e5c77ae3bda70f
Rajkumar Venkatasamy <rajkumar.venkatasamy@navis.com>
2024-10-16 10:00:45 +0530

    initial commit

--- standalone_python_scripts/config.py
+++ standalone_python_scripts/config.py
@@ -1,6 +1,6 @@
 import os
 
-# Set the environment variable "MAILGUN_API_KEY" with your API key
+# Retrieves the API Key from the environment variable
 API_KEY = os.getenv("MAILGUN_API_KEY")
 
 # Mailgun API base URL
@@ -8,4 +8,4 @@ MAILGUN_API_URL = "https://api.mailgun.net/v4/address/validate/bulk"
 
 LIST_NAME = "bulk_mailing_list_validation_1"
 FILE_PATH = "mailing_list.csv"
-COMMAND = "submit_job"  # possible values are "submit_job" and "get_job_status"
+COMMAND = "get_job_status"  # possible values are "submit_job" and "get_job_status"
